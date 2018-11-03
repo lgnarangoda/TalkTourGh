246f41da4f819f1e60b23e5fb6645889c692a1b2
lasitha <narangodalasitha7@gmail.com>
2018-11-03 16:20:13 +0530

    mobile nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,15 +7,15 @@ import {cleanSession} from 'selenium-webdriver/safari';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material';
 import {SigninComponent} from '../signin/signin.component';
 import {AuthServiceLocalService} from '../services/auth-service-local.service';
-import {Component, OnInit} from '@angular/core';
-
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit, AfterViewInit {
   isLoggedIn$: Observable<boolean>;
   nativeWindow: any;
 
@@ -28,6 +28,7 @@ export class HeaderComponent implements OnInit{
 
   ngOnInit() {
     this.isLoggedIn$ = this.authGuard.isLoggedIn; // {2}
+
   }
 
   signOut() {
@@ -35,7 +36,7 @@ export class HeaderComponent implements OnInit{
     this.authService.removeToken();
     this.router.navigate(['home-page']);
     this.authGuard.signOut();
-
+    this.mobileNavigation();
   }
 
   listPropert(type: string) {
@@ -49,8 +50,10 @@ export class HeaderComponent implements OnInit{
   }
 
 
-  openDialog() {
+  signIn() {
     this.dialog.open(SigninComponent, {});
+    // this.mobileNavigation();
+    // console.log("dsddwewe")
   }
 
   navigateResult() {
@@ -61,7 +64,73 @@ export class HeaderComponent implements OnInit{
     this.router.navigate([{outlets: {sidebar: 'add-property'}}]);
   }
 
+  ngAfterViewInit(): void {
 
+this.mobileNavigation();
+
+    const self = this;
+
+    $('.signin').click(function () {
+      self.signIn();
+    });
+
+    $('.myproperty').click(function () {
+      self.navigateResult();
+    });
+
+    $('.addproperty').click(function () {
+      self.navigateAddProperty();
+    });
+
+    $('.signout').click(function () {
+      self.signOut();
+    });
+
+
+  }
+
+  mobileNavigation() {
+    // Mobile Navigation
+    if ($('#nav-menu-container').length) {
+      const $mobile_nav = $('#nav-menu-container').clone().prop({
+        id: 'mobile-nav'
+      });
+      $mobile_nav.find('> ul').attr({
+        'class': '',
+        'id': ''
+      });
+      $('header').append($mobile_nav);
+      $('header').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
+      $('header').append('<div id="mobile-body-overly"></div>');
+      $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
+
+      $(document).on('click', '.menu-has-children i', function (e) {
+        $(this).next().toggleClass('menu-item-active');
+        $(this).nextAll('ul').eq(0).slideToggle();
+        $(this).toggleClass('fa-chevron-up fa-chevron-down');
+      });
+
+      $(document).on('click', '#mobile-nav-toggle', function (e) {
+        $('header').toggleClass('mobile-nav-active');
+        $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+        $('#mobile-body-overly').toggle();
+      });
+
+      $(document).click(function (e) {
+        const container = $('#mobile-nav, #mobile-nav-toggle');
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+          if ($('header').hasClass('mobile-nav-active')) {
+            $('header').removeClass('mobile-nav-active');
+            $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+            $('#mobile-body-overly').fadeOut();
+          }
+        }
+      });
+    } else if ($('#mobile-nav, #mobile-nav-toggle').length) {
+      $('#mobile-nav, #mobile-nav-toggle').hide();
+    }
+
+  }
 
 }
 
--- src/lib/js/main.js
+++ src/lib/js/main.js
@@ -36,6 +36,42 @@ jQuery(document).ready(function( $ ) {
   });
 
 
+
+  // Smooth scroll for the menu and links with .scrollto classes
+  $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
+    event.preventDefault();
+    if (location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        var top_space = 0;
+
+        if ($('#header').length) {
+          top_space = $('#header').outerHeight();
+
+          if( ! $('#header').hasClass('header-fixed') ) {
+            top_space = top_space - 20;
+          }
+        }
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - top_space
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu').length) {
+          $('.nav-menu .menu-active').removeClass('menu-active');
+          $(this).closest('li').addClass('menu-active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('#mobile-body-overly').fadeOut();
+        }
+        return false;
+      }
+    }
+  });
+
   // // Modal video
   // new ModalVideo('.js-modal-btn', {channel: 'youtube'});
 
@@ -59,7 +95,5 @@ jQuery(document).ready(function( $ ) {
   });
 
 // custom code
-  $(".te").click(function(){
-    alert("It is working");
-  });
+
 });
